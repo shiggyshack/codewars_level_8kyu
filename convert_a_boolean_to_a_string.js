97ce04ec866bea3ddf0ad656245c76751554e414
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-12 21:30:17 -0400

    Add files via upload

---
+++ convert_a_boolean_to_a_string.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+    //Checks first to see if the boolean value is 1, if so instructions are executed
+    if(b)
+      {
+        return "true";
+      }
+    //Otherwise, these instructions are executed
+    else {
+      return "false";
+    }
+  }
