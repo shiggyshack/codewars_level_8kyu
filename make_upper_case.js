4d4db9f0d6532928f53aff9c1b19c460be877869
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-13 12:42:26 -0400

    Add files via upload

---
+++ make_upper_case.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    //create new string to hold the upper case version of str
+    let nowUpper = str.toUpperCase()
+    return nowUpper
+  }
