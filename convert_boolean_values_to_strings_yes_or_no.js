2fcc760539da68bb6794b7328b45d19a941f495a
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-12 21:35:07 -0400

    Add files via upload

---
+++ convert_boolean_values_to_strings_yes_or_no.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//One function using a if/else conditional structure to determine between two stings to return.
+
+function boolToWord( bool ){
+    if(bool){
+      return "Yes"
+    }
+    else{
+      return "No"
+    }
+  }
