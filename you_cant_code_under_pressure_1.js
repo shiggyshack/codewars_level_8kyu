94d1f6e258e6f9a38ac6345cb2044d81d6141e07
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-14 12:38:47 -0400

    Add files via upload

---
+++ you_cant_code_under_pressure_1.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i *= 2;
+    return i;
+  }
