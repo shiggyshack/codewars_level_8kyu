f75befb56497976c506b33a804cd051f21439040
Sikes <99853518+shiggyshack@users.noreply.github.com>
2022-10-20 16:27:47 -0400

    Add files via upload

---
+++ calculate_average.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    if(array.length < 1){
+      return 0
+    }
+    else{
+      let sum = 0
+      for(let i = 0; i < array.length; i++){
+        sum += array[i]
+      }
+      return sum/array.length
+    }
+  }
