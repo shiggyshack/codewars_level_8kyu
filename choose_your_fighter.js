efec184242f77c3e74ef997a8e6563554c6172a0
Sikes <99853518+shiggyshack@users.noreply.github.com>
2023-02-18 12:33:00 -0500

    Create choose_your_fighter.js

---
+++ choose_your_fighter.js
@@ -0,0 +1,11 @@
+//Write a quote function that takes a parameter of a fighter's name and returns a string depending on the fighter whose name is entered.
+var quote = function(fighter) {
+  //Use toLowerCase() so the string will not be case sensitive.
+  if(fighter.toLowerCase() === 'george saint pierre'){
+    return 'I am not impressed by your performance.'   
+  }
+  //Use toLowerCase() so the string will not be case sensitive.
+  if(fighter.toLowerCase()=== 'conor mcgregor'){
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+};
