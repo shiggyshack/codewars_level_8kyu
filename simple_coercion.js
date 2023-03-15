820557bb63c5330bf27cd183c845c3746bfe1d83
Sikes <99853518+shiggyshack@users.noreply.github.com>
2023-03-15 14:12:15 -0400

    Create simple_coercion.js

---
+++ simple_coercion.js
@@ -0,0 +1,11 @@
+//Create a function, that accepts two parameters: one a number, one a string, and return true if they are the same, just not the same data type; otherwise, return false.
+function add(a, b){
+  //The '==' will be used to determine if the implicit values of the varibles are equal without having to convert data types.
+  if(a == b){
+    return true
+  }
+  //Return 'false' if this comparision doesn't hold true.
+  else{
+    return false
+  }
+}
